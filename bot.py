6826ba255c771b46962b2f15d9cf048ae611e468
Acidion <acidburn947@gmail.com>
2020-06-17 20:31:51 -1000

    Changed string formats
    Changed formats for strings and updated command prefix to avoid triggering emoji selection.

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_LOADOUT_TOKEN')
 
-bot = commands.Bot(command_prefix=':')
+bot = commands.Bot(command_prefix='$')
 
 @bot.event
 async def on_ready():
-    print(f'{bot.self} has connected to Discord!')
+    print('{} has connected to Discord!'.format(bot.self))
 
     
 @bot.event
@@ -28,7 +28,7 @@ async def test(ctx):
 async def on_error(event, *args, **kwargs):
     with open('err.log', 'a') as f:
         if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
+            f.write('Unhandled message: {}\n'.format(args[0]))
         else:
             raise
             
