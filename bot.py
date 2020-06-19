8aa1c5c90ad6543301f4537af71b68d34a3f517b
Acidion <acidburn947@gmail.com>
2020-06-18 17:32:14 -1000

    Append is stupid
    And I don't respect it. So I've switched to insert.

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ async def add(ctx, *args):
                 exists = True
     if not exists:
         with open(JSON_FILE, 'w') as outfile:
-            loadouts.append(temploadout)
+            loadouts.insert(len(loadouts), temploadout)
             saveJSON
             response = 'Loadout {} added to repository.'.format(loadoutName)
     else:
